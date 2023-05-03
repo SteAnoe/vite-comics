f01768eec8bb7ad8045eec5c3e42d3d7df2d422f
SteAnoe <ste.anoe@gmail.com>
2023-05-03 15:14:37 +0200

    stage 1 - header

---
+++ src/components/Header.vue
@@ -0,0 +1,88 @@
+<script>
+    export default{
+        name: "Header",
+        data() {
+            return {
+                currentActive: 0,
+                menuLinks: [
+                    {
+                        nome: 'CHARACTERS',
+                        url: '/',
+                        current: false
+                    },
+                    {
+                        nome: 'COMICS',
+                        url: '/comics',
+                        current: true
+                    },
+                    {
+                        nome: 'MOVIES',
+                        url: '/movies',
+                        current: false
+                    },
+                    {
+                        nome: 'TV',
+                        url: '/tv',
+                        current: false
+                    },
+                    {
+                        nome: 'GAMES',
+                        url: '/games',
+                        current: false
+                    },
+                    {
+                        nome: 'COLLECTIBLES',
+                        url: '/collectibles',
+                        current: true
+                    },
+                    {
+                        nome: 'VIDEOS',
+                        url: '/videos',
+                        current: false
+                    },
+                    {
+                        nome: 'FANS',
+                        url: '/fans',
+                        current: false
+                    },
+                    {
+                        nome: 'NEWS',
+                        url: '/news',
+                        current: false
+                    },
+                    {
+                        nome: 'SHOP',
+                        url: '/shop',
+                        current: false
+                    }
+                ]
+            }   
+        },
+        methods: {
+            changeCurrent(index){
+                this.currentActive = index
+            }
+        }
+    }
+</script>
+
+<template>
+  <header>
+    <div class="container">
+       <div>
+            <img src="/img/dc-logo.png" alt="">
+        </div>
+        <nav>
+            <ul>
+                <li v-for="(elem, index) in menuLinks" :key="index" @click="changeCurrent(index)" :class="(currentActive == index) ? 'active' : '' ">
+                    <a :class="(currentActive == index) ? 'active-bg' : '' "><strong>{{ elem.nome }}</strong></a>
+                </li>
+            </ul>
+        </nav> 
+    </div>
+  </header>
+</template>
+
+<style lang="scss" scoped>
+
+</style>
---
+++ src/components/Main.vue
@@ -0,0 +1,54 @@
+<script>
+    export default{
+        name: "Main",
+        data() {
+            return {
+                menuShop: [
+                    {
+                        nome: 'DIGITAL COMICS',
+                        img: '/img/buy-comics-digital-comics.png'
+                    },
+                    {
+                        nome: 'DC MERCHANDISE',
+                        img: '/img/buy-comics-merchandise.png' 
+                    },
+                    {
+                        nome: 'SUBSCRIPTION',
+                        img: '/img/buy-comics-subscriptions.png'
+                    },
+                    {
+                        nome: 'COSMIC SHOP LOCATOR',
+                        img: '/img/buy-comics-shop-locator.png'
+                    },
+                    {
+                        nome: 'DC POWER VISA',
+                        img: '/img/buy-dc-power-visa.svg'
+                    }
+                ]
+                
+            }   
+        }
+        
+    }
+</script>
+
+<template>
+  <main>
+    <div id="content">
+        <div class="container">
+            Content goes Here
+        </div>  
+    </div>
+    <div id="shop">
+        <div class="container">
+            <div v-for="(elem, index) in menuShop">
+                <img src="" alt="">
+
+            </div>
+        </div>
+    </div>
+  </main>
+</template>
+
+<style lang="scss" scoped>
+</style>
